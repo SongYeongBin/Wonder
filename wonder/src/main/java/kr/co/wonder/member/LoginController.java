bdcb0c38ad31eb13d449232ecdf1c44b73c28e73
SongYeongBin <109274306+SongYeongBin@users.noreply.github.com>
2022-12-26 17:38:33 +0900

    프로젝트 wonder

--- wonder/src/main/java/kr/co/wonder/member/LoginController.java
+++ wonder/src/main/java/kr/co/wonder/member/LoginController.java
@@ -67,13 +67,14 @@ public class LoginController {
 }
 		
 	//로그인
-
 	@RequestMapping (value = "member/login", method=RequestMethod.GET)
-			public String login() {
-			
-			return "member/login";
+	public String login() {
+		
+		return "member/login";
 	}
 	
+
+	// 로그인 시 회원일 시 일반 페이지, 관리자일 시 관리자 페이지 이동 (등급으로 체크)
 	@RequestMapping(value = "member/loginProc", method=RequestMethod.POST)
 		public String login(MemberDTO memberDTO, HttpServletRequest request, RedirectAttributes rttr) throws Exception{
 		request.setCharacterEncoding("utf-8");
@@ -82,6 +83,7 @@ public class LoginController {
 		HttpSession session = request.getSession();
 		
 		MemberDTO user = service.login(memberDTO);
+		
 		logger.info("data : " + user);
 		if(user == null) {
 			
@@ -97,8 +99,6 @@ public class LoginController {
 		return "redirect:/";
 	}
 		
-	
-
 	// 로그아웃
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(HttpServletRequest request) throws Exception{
@@ -112,99 +112,73 @@ public class LoginController {
 	}
 	
 	// 아이디찾기 폼
-	@RequestMapping(value = "member/idfindView", method=RequestMethod.GET)
-		public String idfindView() throws Exception{
-			logger.info("아이디찾기");
-			return "member/idfindView";
-	}
-	
-	// 아이디찾기
-	@RequestMapping (value = "member/idfind", method=RequestMethod.POST)
-		 	public ModelAndView idfind(@RequestParam("mem_name")String mem_name,@RequestParam("mem_phonenum")int mem_phonenum, Map<String,Object> map, 
-		 			Model model, HttpServletRequest request ) throws Exception {
-				request.setCharacterEncoding("utf-8");
-				logger.info("mem_name,"+"mem_phonenum"+mem_name+mem_phonenum);
-					map.put("mem_name", mem_name);
-					map.put("mem_phonenum", mem_phonenum);
-				ModelAndView mav = new ModelAndView();
-				String id= service.idfind(map);
-				mav.setViewName("member/idfind");
-				mav.addObject("id", id);
+			@RequestMapping(value = "member/idfindView", method=RequestMethod.GET)
+				public String idfindView() throws Exception{
+					logger.info("아이디찾기");
+					return "member/idfindView";
+			}
+			
+			// 아이디찾기
+			@RequestMapping (value = "member/idfind", method=RequestMethod.POST)
+				 	public ModelAndView idfind(@RequestParam("mem_name")String mem_name,@RequestParam("mem_phonenum")int mem_phonenum, Map<String,Object> map, 
+				 			Model model, HttpServletRequest request ) throws Exception {
+						request.setCharacterEncoding("utf-8");
+						logger.info("mem_name,"+"mem_phonenum"+mem_name+mem_phonenum);
+							map.put("mem_name", mem_name);
+							map.put("mem_phonenum", mem_phonenum);
+						ModelAndView mav = new ModelAndView();
+						String id= service.idfind(map);
+						mav.setViewName("member/idfind");
+						mav.addObject("id", id);
+						 
+						logger.info("id="+service.idfind(map));
+						return mav;
+			} // end of idfind
+					
+							
+							
+			// 비밀번호 찾기 폼
+			@RequestMapping(value = "member/pwsFindView", method=RequestMethod.GET)
+			public String pwsFindView() throws Exception{
+				logger.info("비밀번호찾기");
+				return "member/pwsFindView";
+			}
 				
-					
-					logger.info("id="+service.idfind(map));
-					return mav;
-					} // end of idfind
-					
-		// 비밀번호 찾기 폼
-	@RequestMapping(value = "member/pwsFindView", method=RequestMethod.GET)
-	public String pwsFindView() throws Exception{
-		logger.info("비밀번호찾기");
-		return "member/pwsFindView";
-	}
-		
-	// 비밀번호 수정
-	@RequestMapping(value="member/pwsfind", method=RequestMethod.POST)
-		public String pwsfind(@RequestParam("mem_id")String mem_id,@RequestParam("mem_hint1")String mem_hint1,Map<String,Object>map,Model model,
-				@RequestParam("mem_hint2") String mem_hint2) throws Exception{
-				logger.info(mem_id+mem_hint1+mem_hint2);
-				map.put("mem_id", mem_id);
-				map.put("mem_hint1", mem_hint1);
-				map.put("mem_hint2", mem_hint2);
+			// 비밀번호 수정
+			@RequestMapping(value="member/pwsfind", method=RequestMethod.POST)
+				public String pwsfind(@RequestParam("mem_id")String mem_id,@RequestParam("mem_hint1")String mem_hint1,Map<String,Object>map,Model model,
+						@RequestParam("mem_hint2") String mem_hint2) throws Exception{
+						logger.info(mem_id+mem_hint1+mem_hint2);
+						map.put("mem_id", mem_id);
+						map.put("mem_hint1", mem_hint1);
+						map.put("mem_hint2", mem_hint2);
+						
+						int r = service.pwsfind(map);
+						if(r==0) {
+							model.addAttribute("msg", "아이디 또는 힌트를 확인해주세요");
+							
+							return "member/pwsFindView";	
+						} 
+						model.addAttribute("map",map);
+						logger.info(""+map);
+						return "member/pwupdate";
 				
-				int r = service.pwsfind(map);
-				if(r==0) {
-					model.addAttribute("msg", "아이디또는 힌트를 확인해주세요");
+			}
+				// 비밀번호 저장
+				@RequestMapping(value="member/pwupdate", method = RequestMethod.POST)
+				public String pwupdate(@RequestParam("mem_id")String mem_id, @RequestParam("mem_password")String mem_password, MemberDTO memberDTO,HttpSession session,
+					RedirectAttributes rttr, HttpServletRequest request)throws Exception{
+					request.setCharacterEncoding("utf-8");
 					
-					return "member/pwsFindView";	
-				} 
-				model.addAttribute("map",map);
-				logger.info(""+map);
-				return "member/pwupdate";
-		
-	}
-		// 비밀번호 저장
-		@RequestMapping(value="member/pwupdate", method = RequestMethod.POST)
-		public String pwupdate(@RequestParam("mem_id")String mem_id, @RequestParam("mem_password")String mem_password, MemberDTO memberDTO,HttpSession session,
-			RedirectAttributes rttr, HttpServletRequest request)throws Exception{
-			request.setCharacterEncoding("utf-8");
-			
-			memberDTO.setMem_id(mem_id);
-			memberDTO.setMem_password(mem_password);
-			int r=service.pwupdate(memberDTO);
-			
-			logger.info(""+mem_id+mem_password);
-			session.invalidate();
-			rttr.addFlashAttribute("result", "updateOK");
-			return "member/login";
-//			int r = service.pwupdate(memberDTO);
-//			if (r == 0) {
-//				rttr.addFlashAttribute("msg", "수정 성공");
-//				logger.info("성공");
-//				return "login";
-//				
-//			}else {
-//				
-//				return "redirect:pwsFindView";
-//			}
-			
-		}
-	
-		//마이페이지 프로필 보기
-		@RequestMapping(value="mypage/mypage", method=RequestMethod.GET)
-		public String mypage(MemberDTO memberDTO, Model model,HttpSession session) throws Exception{
-			logger.info("진입");
-			String id= (String) session.getAttribute("mem_id");
-			MemberDTO user = service.mypage(id);
-			model.addAttribute("user", service.mypage(id));
-			
-			
-			
-			return "mypage/mypage";
-		}
-		
+					memberDTO.setMem_id(mem_id);
+					memberDTO.setMem_password(mem_password);
+					int r=service.pwupdate(memberDTO);
+					
+					logger.info(""+mem_id+mem_password);
+					session.invalidate();
+					rttr.addFlashAttribute("result", "비밀번호 수정이 완료되었습니다");
+					return "member/login";
+				}
 
 	
-
-
-}
+			}
